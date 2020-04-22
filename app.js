ff46d4a23c50025ae8c5004f9c3394f5b38512c1
unknown <256pasternakmarina@gmail.com>
2020-04-22 20:18:06 +0300

    calendar

--- app.js
+++ app.js
@@ -59,6 +59,8 @@ let listGlobalArray={'user':Users=[],
                     'model':Models=[],
                     'car':Cars=[]}
 
+//data for calendar                   
+
 let addForAdmin=document.querySelectorAll('.for-admin');
 let hashName = window.location.hash.toString();
 //check that input date is correct
@@ -188,29 +190,28 @@ function LogInSubmit()
     }
     
 }
-
+console.log()
 function changeForm()
 {
     
-    let formR=document.getElementById('Regestr');
-    let formL=document.getElementById("LogIn");
-    formL.style.display='none';
-    formR.style.display='none';
-    if(hashName =='#Regestr')
+    let formRegestr=document.querySelectorAll(".forms form");
+    let btnChange=document.querySelectorAll(".forms a");
+    btnChange[0].onclick=function()
     {
-         formR.style.display='';
-         formL.style.display='none';
+        formRegestr[1].style.display='';
+        formRegestr[0].style.display='none';
     }
-    else
+    btnChange[1].onclick=function()
     {
-        formL.style.display='';
-        formR.style.display='none';
+        formRegestr[0].style.display='';
+        formRegestr[1].style.display='none';
     }
 
 }
 
 function hideDOM()
 {
+    
     document.getElementById('Regestr').style.display='none';
     document.getElementById('mainBlock').style.display='none';
     for(let i=0;i<addForAdmin.length;i++)
@@ -220,6 +221,26 @@ function hideDOM()
     
 } 
 
+function menu()
+{
+    document.getElementsByClassName('arrow-scroll')[0].style.display=''
+    document.getElementById('menu').style.display='none'
+    document.getElementById('btnMenu').addEventListener('click',function(){
+        let showHide=document.getElementById('menu').style.display
+        if(showHide=='none')
+        {
+            document.getElementById('menu').style.display=''
+            document.getElementsByClassName('arrow-scroll')[0].style.display='none'
+        }
+        else
+        {
+            document.getElementById('menu').style.display='none'
+            document.getElementsByClassName('arrow-scroll')[0].style.display=''
+        }
+
+    })
+}
+
 
 //functions which should be call after the body is loaded
 function WorkOnLoad()
@@ -228,6 +249,8 @@ function WorkOnLoad()
     ForSubmit();
     LogInSubmit();
     hideDOM();
+    menu();
+    changeForm();
     for(expr in listGlobalArray)
     {
         FillArrays(listGlobalArray[expr],expr)
@@ -235,14 +258,135 @@ function WorkOnLoad()
     document.getElementById(hashName.slice(1)).style.display=''
     
 }
-//functions which should be call after the hash is chaged
-function WorkOnHashChange()
-{
-    changeForm();
-    
-}
 
   //localStorage.clear();
     document.addEventListener("DOMContentLoaded",WorkOnLoad);
-    window.onhashchange = WorkOnHashChange;
 })();
+
+
+
+
+
+function getWeekDay(date) {
+    let days = ['Mo','Tu','We','Th','Fr','Sa','Su'];
+    return days[date.getDay()];
+}
+
+function numDays(currentYear,currMounth,ulDays)
+{
+    let days = ['Mo','Tu','We','Th','Fr','Sa','Su'];
+    let dayOfFirst = new Date(currentYear, currMounth, 1);
+    let startWeekDay=getWeekDay(dayOfFirst);
+    let month={
+        'January':30,
+        'February':28,
+        'March':31,
+        'April':30,
+        'May':31,
+        'June':30, 
+        'July': 31, 
+        'August': 31, 
+        'September': 30, 
+        'October': 31, 
+        'November': 30, 
+        'December': 31
+    }
+    let mon=Object.keys(month);
+    for(let i=0;i<days.length;i++)
+    {
+        let li=document.createElement('li')
+        li.innerHTML=days[i];
+        ulDays.appendChild(li)
+    }
+    for(let i=1;i<days.indexOf(startWeekDay);i++)
+    {
+        let li=document.createElement('li')
+        li.innerHTML=''
+        ulDays.appendChild(li)
+    }
+    for(let i=1;i<month[mon[currMounth]]+1;i++)
+    {
+        let li=document.createElement('li')
+        li.innerHTML=i;
+        ulDays.appendChild(li)
+    }
+    return ulDays
+}
+
+class calendar extends HTMLElement {
+    constructor() {
+        // Always call super first in constructor
+        super();
+
+        let mon=['January','February','March','April','May',
+        'June','July','August','September','October','November','December'];
+        let currantData=new Date();
+        let currentYear=currantData.getFullYear();
+        let currMounth=currantData.getMonth();
+        const currMonconst=currantData.getMonth();
+        const currYearconst=currantData.getFullYear();
+        let back = document.createElement('span');
+        let backArrow=document.createElement('button');
+        let forwardArrow=document.createElement('button');
+        let divBack=document.createElement('div');
+        let mounth=document.createElement('p');
+        let year=document.createElement('p');
+        let divCalendar=document.createElement('div');
+        let ulDays=document.createElement('ul');
+       
+
+        divCalendar.setAttribute('class','date');
+        ulDays.setAttribute('class','days');
+        back.setAttribute('class','back');
+        backArrow.innerHTML='&lt;';
+        backArrow.style.visibility = "hidden";
+        forwardArrow.innerHTML='&gt;'
+        mounth.innerHTML=mon[currMounth];
+        year.innerHTML=currentYear;
+        ulDays=numDays(currentYear,currMounth,ulDays);
+
+        forwardArrow.onclick=function(){
+            currMounth=currMounth+1
+            backArrow.style.visibility='visible';
+            if(currMounth==12)
+            {
+                monthToMove=0;
+                currentYear=currentYear+1
+            }
+            ulDays.innerHTML = "";
+            ulDays=numDays(currentYear,currMounth,ulDays);
+            mounth.innerHTML=mon[currMounth];
+            year.innerHTML=currentYear;
+        }
+
+        backArrow.onclick=function(){
+            currMounth=currMounth-1
+            if(currMounth==0)
+            {
+                monthToMove=11;
+                currentYear=currentYear-1
+            }
+            if(currMonconst==currMounth &&currYearconst==currentYear)
+            {
+                backArrow.style.visibility = "hidden";
+            }
+            ulDays.innerHTML = "";
+            ulDays=numDays(currentYear,currMounth,ulDays);
+            mounth.innerHTML=mon[currMounth];
+            year.innerHTML=currentYear;
+        }
+
+        divCalendar.appendChild(ulDays);
+        back.appendChild(backArrow)
+        back.appendChild(mounth)
+        back.appendChild(year)
+        back.appendChild(forwardArrow)
+        divBack.append(back)
+        this.append(divBack);
+        this.append(divCalendar);
+        
+        
+    }
+ }
+
+ customElements.define('new-calendar', calendar);
